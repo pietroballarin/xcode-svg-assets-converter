034198d1f6e9f44f18879b71208b229048a6f6a2
Pietro Ballarin <>
2023-08-27 10:57:01 +0200

    Script reads assets from json and appends extension

--- svg-script.rb
+++ svg-script.rb
@@ -1,26 +1,18 @@
 require 'rest-client'
 require 'mini_magick'
 require 'fileutils'
-require 'xcodeproj'
-
-url_to_filename = {
-  'https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/caution.svg' => 'caution.pdf',
-  'https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/compass.svg' => 'check.pdf',
-  'https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/ch.svg' => 'circles.pdf',
-  'https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/cartman.svg' => 'cartman.pdf',
-  'https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/dukechain.svg' => 'duke.pdf',
-  'https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/eff.svg' => 'eff.pdf'
-}
+require 'json'
 
 base_xcode_path = "/Users/pietroballarin/Desktop/SvgTest/SvgTest"
 xcode_project_path = "/Users/pietroballarin/Desktop/SvgTest/SvgTest.xcodeproj"
 
-project = Xcodeproj::Project.open(xcode_project_path)
-target = project.targets.first
+# Read URLs and filenames from the JSON file
+json_data = File.read('/Users/pietroballarin/Desktop/xcode-svg-assets-converter/Assets.json')
+url_and_filename_data = JSON.parse(json_data)
 
-url_to_filename.each do |url, filename_with_extension|
-  # Extract the filename without extension
-  filename = File.basename(filename_with_extension, ".*")
+url_and_filename_data.each do |entry|
+  url = entry['url']
+  filename_without_extension = entry['filename'] # Filename without extension
 
   svg_content = RestClient.get(url)
 
@@ -31,22 +23,17 @@ url_to_filename.each do |url, filename_with_extension|
   image.format 'pdf'
 
   # Calculate the absolute paths for new and existing files
-  imageset_folder = "#{base_xcode_path}/Assets.xcassets/#{filename}.imageset"
+  imageset_folder = "#{base_xcode_path}/Assets.xcassets/#{filename_without_extension}.imageset"
   FileUtils.mkdir_p(imageset_folder)
 
   # Corrected paths for images
-  png_output_path = "#{imageset_folder}/#{filename_with_extension}"
+  pdf_output_filename = "#{filename_without_extension}.pdf"
+  pdf_output_path = "#{imageset_folder}/#{pdf_output_filename}"
 
-  # Write the PNG image to the new output path
-  image.write png_output_path
+  # Write the PDF image to the new output path
+  image.write pdf_output_path
 
-  puts "Conversion complete: #{png_output_path}"
-
-  # Add the image to the Xcode project
-  image_asset = project.new_file(png_output_path)
-
-  # Add the file reference to the resources build phase
-  build_file = target.resources_build_phase.add_file_reference(image_asset)
+  puts "Conversion complete: #{pdf_output_path}"
 
   # Create the Contents.json file for the image set
   contents_json_path = "#{imageset_folder}/Contents.json"
@@ -54,7 +41,7 @@ url_to_filename.each do |url, filename_with_extension|
     file.write(JSON.pretty_generate({
       "images" => [
         {
-          "filename" => filename_with_extension,
+          "filename" => pdf_output_filename, # Use the PDF filename here
           "idiom" => "universal"
         }
       ],
@@ -65,6 +52,3 @@ url_to_filename.each do |url, filename_with_extension|
     }))
   end
 end
-
-# Save the modified project
-project.save
